103da5f448046eb35d159e2a9a535fdbaa3df3cf
dtrihinas <trihinas@cs.ucy.ac.cy>
2015-04-14 15:31:47 +0300

    iii

--- src/main/java/testpack/ServletListener.java
+++ src/main/java/testpack/ServletListener.java
@@ -5,20 +5,20 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 import eu.paasport.services.monitoring.agent.MonitoringAgent;
-//import eu.paasport.services.monitoring.agent.exceptions.MonitoringException;
+import eu.paasport.services.monitoring.agent.exceptions.MonitoringException;
 
 public class ServletListener implements ServletContextListener{
 
     public void contextInitialized(ServletContextEvent event){
     	ServletContext sc = event.getServletContext();  	
 
-//    	try {
-//			MonitoringAgent agent = new MonitoringAgent();
-//	    	sc.setAttribute("agent", agent);
-//		} 
-//    	catch (MonitoringException e) {
-//			e.printStackTrace();
-//		}
+    	try {
+			MonitoringAgent agent = new MonitoringAgent();
+	    	sc.setAttribute("agent", agent);
+		} 
+    	catch (MonitoringException e) {
+			e.printStackTrace();
+		}
     	    
     	System.out.println("ServletListener>> Up and Running");
     }
