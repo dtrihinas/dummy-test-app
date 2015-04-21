3703cacbb7b12186267046f9bea4a8f856ab9118
dtrihinas <trihinas@cs.ucy.ac.cy>
2015-04-21 12:28:42 +0300

    new version

--- src/main/java/testpack/ServletListener.java
+++ src/main/java/testpack/ServletListener.java
@@ -1,33 +1,30 @@
 package testpack;
 
-import java.util.HashMap;
-
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 
-import eu.paasport.services.monitoring.agent.MonitoringAgent;
-import eu.paasport.services.monitoring.agent.exceptions.MonitoringException;
-
-public class ServletListener implements ServletContextListener{
-
-    public void contextInitialized(ServletContextEvent event){
+@WebListener
+public class ServletListener implements ServletContextListener {
+	
+    public void contextInitialized(ServletContextEvent event) {
     	ServletContext sc = event.getServletContext();  	
 
-    	try {
-    		HashMap<String,String> params = new HashMap<String,String>();
-			params.put("api.key", "123456789");
-			params.put("app.name", "myapp1");
-			params.put("agent.name", "myagent");
-			params.put("server.endpoint", "http://83.212.86.242");
-			//params.put("agent.logging", "true");
-			
-			MonitoringAgent agent = new MonitoringAgent(params);
-	    	sc.setAttribute("agent", agent);
-		} 
-    	catch (MonitoringException e) {
-			e.printStackTrace();
-		}
+    	
+//    	try {
+//    		HashMap<String,String> params = new HashMap<String,String>();
+//			params.put("api.key", "123456789");
+//			params.put("app.name", "myapp1");
+//			params.put("agent.name", "myagent");
+//			params.put("server.endpoint", "localhost");
+//			
+//			MonitoringAgent agent = new MonitoringAgent(params);
+//	    	sc.setAttribute("agent", agent);
+//		} 
+//    	catch (MonitoringException e) {
+//			e.printStackTrace();
+//		}
     	    
     	System.out.println("ServletListener>> Up and Running");
     }
--- src/main/java/testpack/TestServlet.java
+++ src/main/java/testpack/TestServlet.java
@@ -5,13 +5,12 @@ import java.io.PrintWriter;
 import java.util.Random;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class TestServlet
- */
+@WebServlet(urlPatterns = "/TestServlet")
 public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
